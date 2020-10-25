3c29e626a8565ee08750197e781c3737ea2f88a9
William Santos <williamdsw@outlook.com>
2020-10-25 19:55:48 -0300

    Code refactoring for LineChart component

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -4,40 +4,39 @@ import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-line-chart',
-  templateUrl: './line-chart.component.html',
-  styleUrls: ['./line-chart.component.css']
+  templateUrl: './line-chart.component.html'
 })
 export class LineChartComponent implements OnInit {
 
   // FIELDS
 
-  lineChartLabels: Label[] = ['Manchester United', 'Chelsea', 'Tottenham', 'Arsenal', 'Liverpool', 'Everton', 'Manchester City', 'Leicester City'];
-  lineChartData: ChartDataSets[] = [
-    { data: [45, 42, 36, 39, 50, 34, 48, 40], label: 'Best Clubs' }
+  public lineChartLabels: Label[] = [
+    'Manchester United', 'Chelsea', 'Tottenham', 'Arsenal', 'Liverpool', 
+    'Everton', 'Manchester City', 'Leicester City'
+  ];
+  public lineChartData: ChartDataSets[] = [
+    { data: [45, 42, 36, 39, 50, 34, 48, 40], label: 'Points' }
   ];
 
-  lineChartOptions: ChartOptions = {
-    responsive: true
-  };
+  public lineChartOptions: ChartOptions = { responsive: true };
 
-  lineChartColors: Color[] = [
+  public lineChartColors: Color[] = [
     {
       borderColor: 'black',
       backgroundColor: 'rgba(255, 255, 0, 0 ,0.28)'
     },
   ];
 
-  lineChartLegend: boolean = true;
-  lineChartPlugins: ChartPluginsOptions[] = [];
-  lineChartType: ChartType = 'line';
+  public lineChartLegend = true;
+  public lineChartPlugins: ChartPluginsOptions[] = [];
+  public lineChartType: ChartType = 'line';
 
   // CONSTRUCTOR
 
   constructor() { }
 
-  // LIFE CYCLE HOOKS 
+  // LIFE CYCLE HOOKS
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
